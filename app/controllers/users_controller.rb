46208cf879fe0edcdcfb0150a056440f165a0528
Chitranshu Bisht <chitranshubisht31@gmail.com>
2024-04-25 12:35:59 +0530

    Resolved MR comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
-
-  def create 
-      user = User.new(permitted_params)
-      if user.save
-        render json: user, status: ok
-      else
-        render json: user.errors, status: unprocessable_entity
-      end
+  def create
+    user = User.new(permitted_params)
+    if user.save
+      render json: user, status: ok
+    else
+      render json: user.errors, status: unprocessable_entity
+    end
   end
 
-  def index 
-    users = User.all
-    render json: users
+  def index
+    users_posts = User.includes(:posts)
+    render json: users_posts.to_json(include: :posts)
   end
 
   private
